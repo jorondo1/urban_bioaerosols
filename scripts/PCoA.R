4708bb72b302444ac0461928d9ed592703c717fc
Jonathan Rondeau-Leclaire <ronj2303@ip34-rockylinux8.m>
2024-12-12 09:02:25 -0500

    full pcoa

--- scripts/PCoA.R
+++ scripts/PCoA.R
@@ -1,15 +1,15 @@
 library(pacman)
-p_load(tidyverse, magrittr, purrr, patchwork)
+p_load(tidyverse, magrittr, purrr, patchwork, grid)
 source("https://github.com/jorondo1/misc_scripts/raw/refs/heads/main/community_functions.R")
 source("https://github.com/jorondo1/misc_scripts/raw/refs/heads/main/tax_glom2.R")
 source("https://github.com/jorondo1/misc_scripts/raw/refs/heads/main/rarefy_even_depth2.R")
 
 urbanbio.path <- '~/Desktop/ip34/urbanBio'
 
-ps.ls <- read_rds(file.path(urbanbio.path, 'data/ps.list.rds'))
+ps.ls <- read_rds(file.path(urbanbio.path, 'data/ps.ls.rds'))
 cities <- ps.ls$BACT@sam_data$city %>% unique
 seasons <- c('Spring' = 'springgreen3', 'Summer' = 'skyblue3', 'Fall' = 'violetred3')
-barcodes <- c('BACT', 'FUNG', 'PLAN')
+barcodes <- c('BACT' = 'Bacteria', 'FUNG' = 'Fungi', 'PLAN' = 'Plants')
 
 ps_rare.ls <- lapply(ps.ls, function(ps) {
   set.seed(1234)
@@ -43,6 +43,59 @@ pcoa_bray_byCity.ls <- imap(
   })
 })
 
+# Mega dataframe with all results
+pcoa.df <- imap(pcoa_bray_byCity.ls, function(pcoa.ls, city) {
+  imap(pcoa.ls, function(pcoa, barcode) {
+    pcoa$metadata %>% 
+      rownames_to_column('Sample') %>% 
+      select(Sample, city, time, PCo1, PCo2) %>% 
+      mutate(barcode = barcode)
+  }) %>% list_rbind
+}) %>% list_rbind
+
+# Eigenvalues df
+eig.df <- imap(pcoa_bray_byCity.ls, function(pcoa.ls, city) {
+  imap(pcoa.ls, function(pcoa, barcode) {
+    pcoa$eig %>% 
+      data.frame(Eig = .) %>% 
+      rownames_to_column('MDS') %>% 
+      tibble %>% 
+      mutate(barcode = barcode,
+             city = city) %>% 
+      group_by(barcode, city) %>% 
+      mutate(Eig = 100*Eig/sum(Eig)) %>% 
+      filter(MDS %in% c('MDS1', 'MDS2'))
+    }) %>% list_rbind
+}) %>% list_rbind %>%
+  group_by(barcode, city, MDS) %>%
+  mutate(barcode = recode(barcode, !!!barcodes),
+         MDS = case_when(MDS == 'MDS1' ~ 'PCo1',
+                         MDS == 'MDS2' ~ 'PCo2')) %>% 
+  summarize(Eig = paste0(MDS, ": ", round(Eig, 1), "%"), .groups = "drop") %>%
+  pivot_wider(names_from = MDS, values_from = Eig)
+
+pcoa.df %>% 
+  mutate(time = factor(time, levels = names(seasons)),
+         barcode = recode(barcode, !!!barcodes)) %>% 
+  ggplot(aes(x = PCo1, y = PCo2, colour = time)) +
+  geom_point(size = 1) +
+  stat_ellipse(level = 0.95, geom = 'polygon', 
+               alpha = 0.2, aes(fill = time)) +
+  theme_light() +
+  facet_grid(barcode~city, scales = 'free') +
+  scale_colour_manual(values = seasons) +
+  scale_fill_manual(values = seasons) +
+  geom_text(data = eig.df, 
+            aes(x = -Inf, y = Inf, label = paste0(PCo1, ", ", PCo2)),
+            inherit.aes = FALSE,
+            hjust = -0.1, vjust = 35) +
+  labs(colour = 'Season', fill = 'Season')
+
+ggsave('~/Desktop/ip34/urbanBio/out/pcoa_season.pdf',
+       bg = 'white', width = 2200, height = 2000, 
+       units = 'px', dpi = 220)
+
+
 # FUNCTION PLOT PCOA
 plot_pcoa <- function(pcoa.ls, ellipse) {
   # extract pcoa eigenvalues
@@ -63,7 +116,7 @@ plot_pcoa <- function(pcoa.ls, ellipse) {
 }
 
 # Example usage:
-plot_pcoa(pcoa_bray.ls$BACT, "city")
+plot_pcoa(pcoa_bray.ls$BACT, "time")
 
 # Plot pcoas, save plots in list
 pcoa_bray_byCity.plot <- imap(
@@ -75,44 +128,6 @@ pcoa_bray_byCity.plot <- imap(
     )
   }
 )
-
-
-column_headers <- map(cities, ~ ggplot() + 
-                        ggtitle(.x) + 
-                        theme_void() + 
-                        theme(plot.title = element_text(hjust = 0.5, size = 14, face = "bold")))
-
-# Add row headers (lvl2 names)
-row_labels <- map(barcodes, ~ ggplot() + 
-                    ggtitle(.x) + 
-                    theme_void() + 
-                    theme(plot.title = element_text(angle = 90, hjust = 0.5, size = 14, face = "bold")))
-
-
-# Create plots for the main grid
-# plot_rows <- map(pcoa_bray_byCity.plot, ~wrap_plots(.x)) # Combine plots for each lvl1
-# 
-# # Combine row labels, main grid, and column headers
-# main_grid <- wrap_plots(
-#   c(
-#     list(wrap_plots(column_headers, ncol = length(column_headers))),  # Top row with column headers
-#     map2(row_labels, plot_rows, ~wrap_plots(list(.x, .y), ncol = 1))     # Add row labels to each row
-#   ),
-#   ncol = 1
-# ) +
-#   plot_layout(heights = c(0.1, rep(1, length(row_labels)))) +  # Adjust height ratios
-#   plot_annotation(title = "Grid of Plots")
-# 
-# # Display the grid
-# main_grid
-# 
-# 
-# 
-# # Combine plots within each city
-# patchwork_grid <- map(pcoa_bray_byCity.plot, ~wrap_plots(.x)) %>%  # Create grids for each lvl1
-#   wrap_plots() +                                          # Combine all grids
-#   plot_annotation(title = "Grid of Plots", tag_levels = "A")
-# 
 
 
 
--- scripts/community_overview.R
+++ scripts/community_overview.R
@@ -7,14 +7,14 @@ source('https://raw.githubusercontent.com/jorondo1/misc_scripts/refs/heads/main/
 source('Desktop/ip34/urbanBio/scripts/myFunctions.R')
 
 tax_ranks <- c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus")
-ps_trnL <- readRDS('~/Desktop/ip34/urbanBio/data/ps.ls.rds')
-
+ps.ls <- readRDS('~/Desktop/ip34/urbanBio/data/ps.ls.rds')
+ps_ctrl.ls <- readRDS('~/Desktop/ip34/urbanBio/data/ps_ctrl.ls.rds')
 ###################################
 # Community composition overview ###
 #####################################
 
 which_taxrank <- 'Genus'
-melted <- ps_trnL$PLAN %>% 
+melted <- ps.ls$PLAN %>% 
   tax_glom(taxrank = which_taxrank) %>% 
   psmelt %>%
   filter(Abundance != 0) %>% 
@@ -58,3 +58,47 @@ ggsave(paste0('~/Desktop/ip34/urbanBio/out/composition_',which_taxrank,'.pdf'),
 ###################################
 # Controls ###
 #####################################
+
+which_taxrank <- 'Family'
+
+control.melt <- imap(ps_ctrl.ls, function(ps, barcode) {
+  ps %>% 
+    tax_glom(taxrank = which_taxrank) %>% 
+    psmelt %>% 
+    select(Sample, Abundance, site_id, !!sym(which_taxrank)) %>% 
+    filter(Abundance != 0) %>% 
+    group_by(Sample) %>% 
+    mutate(relAb = Abundance/sum(Abundance),
+           barcode = barcode)
+}) %>% list_rbind
+
+# Compute top taxa and create "Others" category
+nTaxa <- 14
+(top_taxa <- topTaxa(control.melt, which_taxrank, nTaxa))
+top_taxa_lvls <- top_taxa %>% 
+  group_by(aggTaxo) %>% 
+  aggregate(relAb ~ aggTaxo, data = ., FUN = sum) %>% 
+  arrange(relAb) %$% aggTaxo %>% 
+  as.character %>% # Others first:
+  setdiff(., c('Others', 'Unclassified')) %>% c('Others', 'Unclassified', .)
+
+# Plot !
+expanded_palette <- colorRampPalette(brewer.pal(12, 'Paired'))(nTaxa+2) 
+
+control.melt %>% 
+  left_join(top_taxa %>% select(-relAb), by = which_taxrank) %>%
+  mutate(aggTaxo = factor(aggTaxo, levels = top_taxa_lvls)) %>% 
+  ggplot(aes(y = Sample, x = Abundance, fill = aggTaxo)) +
+  geom_col() +
+  theme_light() +
+  facet_grid(~barcode, 
+            scales = 'free') +
+  scale_fill_manual(values = expanded_palette) +
+  labs(fill = which_taxrank) +
+  theme(panel.grid = element_blank(),
+        axis.ticks.x = element_blank(),
+        panel.border = element_blank())
+
+ggsave(paste0('~/Desktop/ip34/urbanBio/out/composition_',which_taxrank,'_controls.pdf'),
+       bg = 'white', width = 3600, height = 2000, 
+       units = 'px', dpi = 220)
