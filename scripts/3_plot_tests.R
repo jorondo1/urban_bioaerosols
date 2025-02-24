a0bcb3e0f95d3743fbd0cc29d370721a1e1faa95
Jonathan Rondeau-Leclaire <jonathan.rondeau-leclaire@usherbrooke.ca>
2025-02-24 11:03:18 -0500

    init

---
+++ scripts/3_plot_tests.R
@@ -0,0 +1,26 @@
+
+
+#########################################
+# 2. Community composition barcharts #####
+###########################################
+# BFP grid / wrap, MQS rows, mean per time period
+# Time period represented by ~median date ? distinct by city
+
+
+### From here on, individual plots will be patched together
+### per city, to contain both diversity analyses as well as
+### some differential testing and maybe nestedness? 
+
+#########################
+# 3. Alpha diversity #####
+###########################
+# One plot grid per city
+# BFP columns, Idx rows
+
+
+#########################
+# 4. Beta diversity ######
+###########################
+# 
+# 
+# Include variance partitioning?
