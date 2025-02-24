9d9fddebe731edcd80fa7690fc175f68e8704370
Jonathan Rondeau-Leclaire <jonathan.rondeau-leclaire@usherbrooke.ca>
2025-02-24 12:52:01 -0500

    notes, structure

--- scripts/3_plot_tests.R
+++ scripts/3_plot_tests.R
@@ -1,9 +1,13 @@
-
+#######################################
+# X3. Taxonomic classification rates ###
+#######################################
+# Informs following plots on which level to focus
 
 #########################################
 # 2. Community composition barcharts #####
 ###########################################
-# BFP grid / wrap, MQS rows, mean per time period
+# Which level ??
+# BFP grid / wrap with nested period, MQS rows, mean per time period
 # Time period represented by ~median date ? distinct by city
 
 
@@ -21,6 +25,14 @@
 #########################
 # 4. Beta diversity ######
 ###########################
-# 
-# 
+# Betadiv en BFP
+# 2 rows : BC + Aitchison? Ou UniFrac pour les bact?
+# 3rd row : Turnover/nestedness
 # Include variance partitioning?
+
+
+# X4 Community composition all samples 
+# Same as 2., but order by date within facets, time period nested in barcode
+
+
+# X2 Median income vs. vegetation index design (dotplot)
