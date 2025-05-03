34a369d97e984dace03da3379770aad1bc5ff393
Jonathan Rondeau-Leclaire <jonathan.rondeau-leclaire@usherbrooke.ca>
2025-05-03 10:09:26 -0400

    test

--- scripts/3_metrics.R
+++ scripts/3_metrics.R
@@ -1,1 +1,142 @@
 # Diversity metrics for visualisation and statistics
+
+#############
+# X. SETUP ###
+###############
+library(pacman)
+p_load(tidyverse, phyloseq)
+
+# Diversity functions:
+source("https://github.com/jorondo1/misc_scripts/raw/refs/heads/main/community_functions.R")
+urbanbio.path <- '~/Desktop/ip34/urbanBio'
+
+ps_rare.ls <- read_rds(file.path(urbanbio.path, 'data/ps_rare.ls.rds'))
+
+barcodes <- c('BACT' = "Bacteria",
+              'FUNG' = "Fungi",
+              'PLAN' = "Pollen")
+
+periods <- c('Spring' , 'Summer', 'Fall')
+
+cities <- c('Montreal' = 'Montreal', 
+            'Quebec' = 'Quebec', 
+            'Sherbrooke' = 'Sherbrooke')
+
+#######################
+#=== ALPHA DIVERSITY ###
+#########################
+
+# A bunch of indices: 
+diversity.df <- imap(ps_rare.ls, function(ps, barcode) {
+  map(c('Richness', 'Shannon', 'Simpson', 'Tail'), function(index){
+    estimate_diversity(ps, index) %>% 
+      as.data.frame() %>% 
+      setNames(index)
+    }) %>% list_cbind %>% 
+    rownames_to_column('Sample') %>% 
+    mutate(Hill_1 = exp(Shannon),
+           Hill_2 = 1/Simpson) %>% 
+    left_join(samdat_as_tibble(ps), # add samdat
+              by = 'Sample') %>% 
+    mutate(Barcode = barcode,
+           Barcode = recode(Barcode, !!!barcodes),
+           time = factor(time, periods)) %>% 
+    tibble()
+}) %>% list_rbind 
+
+write_rds(diversity.df, file.path(urbanbio.path,'data/diversity/alpha_diversity.rds'))
+
+######################
+#=== BETA DIVERSITY ###
+########################
+
+# Split every dataset by city
+ps_byCity.ls <- lapply(cities, function(city_name) { # 1st level: city
+  lapply(ps_rare.ls, function(ps) { # 2nd level: barcode
+    
+    # Subset samples by city
+    selected_samples <- rownames(sample_data(ps))[sample_data(ps)$city == city_name]
+    
+    # Subset the phyloseq object and remove zero-count taxa
+    prune_samples(selected_samples, ps) %>%
+      prune_taxa(taxa_sums(.) > 0, .)
+  })
+}); names(ps_byCity.ls) <- cities
+
+# Iterate permanova for each city, barcode, distance
+pcoa.ls <- imap(ps_byCity.ls, function(ps.ls, city) {
+  imap(ps.ls, function(ps, barcode){
+    out <- list()
+    out[['robust.aitchison']] <- compute_pcoa(ps, dist = 'robust.aitchison')
+    out[['bray']] <- compute_pcoa(ps, dist = 'bray', vst = TRUE)
+    out
+  })
+})
+
+# Extract first two eigenvalues and create strings for plots
+eig.df <- imap(pcoa.ls, function(pcoa_barcode.ls, city) {
+  imap(pcoa_barcode.ls, function(pcoa_dist.ls, barcode) {
+    imap(pcoa_dist.ls, function(pcoa, dist) {
+      pcoa$eig %>% 
+        data.frame(Eig = .) %>% 
+        rownames_to_column('MDS') %>% 
+        tibble %>% 
+        mutate(Barcode = barcode,
+               City = city,
+               Dist = dist) %>% 
+        group_by(Barcode, City, Dist) %>% 
+        mutate(Eig = 100*Eig/sum(Eig)) %>% # Compute %eig
+        filter(MDS %in% c('MDS1', 'MDS2')) # keep the 1st two
+    }) %>% list_rbind
+  }) %>% list_rbind
+}) %>% list_rbind %>% 
+  group_by(Barcode, City, Dist, MDS) %>%
+  mutate(Barcode = recode(Barcode, !!!barcodes),
+         MDS = case_when(MDS == 'MDS1' ~ 'PCo1',
+                         MDS == 'MDS2' ~ 'PCo2')) %>% 
+  summarize(Eig = paste0(MDS, ": ", round(Eig, 1), "%"), .groups = "drop") %>%
+  pivot_wider(names_from = MDS, values_from = Eig) 
+
+# Compile pcoa data points for plots
+plot.df <- imap(pcoa.ls, function(pcoa_barcode.ls, city) {
+  imap(pcoa_barcode.ls, function(pcoa_dist.ls, barcode) {
+    imap(pcoa_dist.ls, function(pcoa, dist) {
+      pcoa$metadata %>% 
+        rownames_to_column('Sample') %>% 
+        select(Sample, time, PCo1, PCo2) %>% 
+        mutate(Barcode = barcode,
+               Dist = dist,
+               City = city) # add barcode name for each iteration
+    }) %>% list_rbind
+  }) %>% list_rbind
+}) %>% list_rbind %>% tibble %>% 
+  mutate(Barcode = recode(Barcode, !!!barcodes),
+         time = factor(time, levels = periods),
+         # time = recode_factor(time, !!!c(
+         #   'Spring' = '1',
+         #   'Summer' = '2',
+         #   'Fall' = '3')),
+         # time = factor(time, levels = c('1', '2', '3'))
+         )
+
+pcoa.ls$plot.df <- plot.df
+pcoa.ls$eig.df <- eig.df
+
+write_rds(pcoa.ls,  file.path(urbanbio.path,'data/diversity/beta_diversity.ls.rds'))
+
+# Sanity check plot : 
+pcoa.ls$plot.df %>% 
+  filter(Dist == 'bray') %>% 
+  ggplot(aes(x = PCo1, y = PCo2, colour = time)) +
+  geom_point() +
+  stat_ellipse(level = 0.95, geom = 'polygon', 
+               alpha = 0.2, aes(fill = time)) +
+  facet_grid(City ~ Barcode)
+
+
+
+
+
+
+
+
--- scripts/4_plot_tests.R
+++ scripts/4_plot_tests.R
@@ -1,18 +1,16 @@
+# PLOT 2-5 : Community composition + alpha + beta div  (one per city)
+
+#############
+# X. SETUP ###
+###############
 library(pacman)
 p_load(tidyverse, RColorBrewer, phyloseq, patchwork)
 urbanbio.path <- '~/Desktop/ip34/urbanBio'
 
 source(file.path(urbanbio.path, 'scripts/myFunctions.R'))
-source("https://github.com/jorondo1/misc_scripts/raw/refs/heads/main/community_functions.R")
 source("https://github.com/jorondo1/misc_scripts/raw/refs/heads/main/psflashmelt.R")
 source("https://github.com/jorondo1/misc_scripts/raw/refs/heads/main/myFunctions.R")
-source("https://github.com/jorondo1/misc_scripts/raw/refs/heads/main/tax_glom2.R")
 
-#############
-# X. SETUP ###
-###############
-
-# Agglomerate
 ps_rare.ls <- read_rds(file.path(urbanbio.path, 'data/ps_rare.ls.rds'))
 
 barcodes <- c('BACT' = "Bacteria",
@@ -23,72 +21,21 @@ Hill_indices <- c('H_0' = 'Richness',
                   'H_1' = 'exp^(Shannon)',
                   'H_2' = 'Inverse Simpson')
 
-period_colours <- c('Spring' = 'springgreen3',
-                    'Summer' = 'skyblue3', 
-                    'Fall' = 'orange3')
+cities <- c('Montreal' = 'Montreal', 
+            'Quebec' = 'Quebec', 
+            'Sherbrooke' = 'Sherbrooke')
 
-#period_colour = c('Sampling time_period 1' = 'springgreen4', 
-#                  'Sampling time_period 2' = 'skyblue3', 
-#                  'Sampling time_period 3' = 'orange3')
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-#
-#  /$$$$$$$  /$$        /$$$$$$  /$$$$$$$$        /$$$$$$ 
-# | $$__  $$| $$       /$$__  $$|__  $$__/       /$$__  $$
-# | $$  \ $$| $$      | $$  \ $$   | $$         |__/  \ $$
-# | $$$$$$$/| $$      | $$  | $$   | $$           /$$$$$$/
-# | $$____/ | $$      | $$  | $$   | $$          /$$____/ 
-# | $$      | $$      | $$  | $$   | $$         | $$      
-# | $$      | $$$$$$$$|  $$$$$$/   | $$         | $$$$$$$$
-# |__/      |________/ \______/    |__/         |________/
-# 
-#            Community composition barcharts
-#
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-# Dataframe with taxrank mean relative abundance by (taxrank, city, date)
-which_taxrank <- 'Family'
-melted <- imap(ps_rare.ls, function(ps, barcode) {
-  
-  psflashmelt(ps) %>%
-    # filter(Abundance != 0) %>% # KEEP them !
-    # Sum abundance by taxrank for each sample
-    group_by(Sample, date, city, time, !!sym(which_taxrank)) %>% 
-    summarise(Abundance = sum(Abundance), .groups = 'drop') %>%
-    # Then for all samples within a date/city/time combination
-    group_by(date, city, time, !!sym(which_taxrank)) %>% 
-    summarise(Abundance = sum(Abundance), .groups = 'drop') %>%
-    # Compute relab
-    group_by(date, time, city) %>% 
-    mutate(relAb = Abundance/sum(Abundance)) %>% 
-    select(!!sym(which_taxrank), relAb, city, time, date) %>% 
-    ungroup() %>% 
-    mutate(Barcode = barcode, 
-           # To prevent gap in date x axis, we recode date. 
-           date_label = paste(month(date, label = TRUE, abbr=TRUE), 
-                            day(date)),
-         # Recode date as character factor 
-         date = as.character(date))
-}) %>% list_rbind
-
-# Count number of sample by unique (taxrank, city, date) for each barcode
-sample_counts <- imap(ps_rare.ls, function(ps, barcode){
-  samdat_as_tibble(ps) %>% 
-    group_by(date, city, time) %>% 
-    summarise(n_samples = n(), .groups = 'drop') %>% 
-    mutate(Barcode = barcode,
-           date_label = paste(month(date, label = TRUE, abbr=TRUE), 
-                              day(date)),
-           date = as.character(date))
-}) %>% list_rbind
-
+# ==== COLOUR PALETTES
 nTax_by_barcode <- c(
   'BACT' = 14,
   'FUNG' = 10,
   'PLAN' = 8
 )
 
+period_colours <- c('Spring' = 'springgreen3',
+                    'Summer' = 'skyblue3', 
+                    'Fall' = 'orange3')
+nvdi_colours <- c("#E1BE6A", "#40B0A6", "#E66100")
 # Define the bacteria palette with named colors
 palettes <- list() 
 palettes$BACT <- c(
@@ -120,17 +67,17 @@ palettes$FUNG <- c(
   Cortinariaceae="seagreen2",
   Discinellaceae="darkgreen",
   Gloeophyllaceae="palegreen4",
-   Atheliaceae="seagreen",
-   Strophariaceae="darkslategrey",
-   Cerrenaceae="cyan1",
-   Incrustoporiaceae="#00CED1",
-   Ganodermataceae="#40E0D0",
-   Peniophoraceae="#20B2AA",
-   Irpicaceae="#008B8B",
-   Pleosporaceae="#5F9EA0",
-   Fomitopsidaceae="#4682B4",
-   Polyporaceae="#1E90FF",
-   Cladosporiaceae="deepskyblue4")
+  Atheliaceae="seagreen",
+  Strophariaceae="darkslategrey",
+  Cerrenaceae="cyan1",
+  Incrustoporiaceae="#00CED1",
+  Ganodermataceae="#40E0D0",
+  Peniophoraceae="#20B2AA",
+  Irpicaceae="#008B8B",
+  Pleosporaceae="#5F9EA0",
+  Fomitopsidaceae="#4682B4",
+  Polyporaceae="#1E90FF",
+  Cladosporiaceae="deepskyblue4")
 
 palettes$PLAN <- c(
   Others="#C6C2C2",
@@ -144,11 +91,69 @@ palettes$PLAN <- c(
   Asteraceae="#006D2C",
   Pinaceae="#00441B")
 
-cities <- c('Montreal' = 'Montreal', 
-            'Quebec' = 'Quebec', 
-          'Sherbrooke' = 'Sherbrooke')
+#period_colour = c('Sampling time_period 1' = 'springgreen4', 
+#                  'Sampling time_period 2' = 'skyblue3', 
+#                  'Sampling time_period 3' = 'orange3')
 
-# Create all plots 
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# 
+#   /$$$$$$$ /$$       /$$$$$$ /$$$$$$$$/$$$$$$         /$$$$$$       /$$$$$$$ 
+#  | $$__  $| $$      /$$__  $|__  $$__/$$__  $$       /$$__  $$     | $$____/ 
+#  | $$  \ $| $$     | $$  \ $$  | $$ | $$  \__/      |__/  \ $$     | $$      
+#  | $$$$$$$| $$     | $$  | $$  | $$ |  $$$$$$         /$$$$$$/$$$$$| $$$$$$$ 
+#  | $$____/| $$     | $$  | $$  | $$  \____  $$       /$$____|______|_____  $$
+#  | $$     | $$     | $$  | $$  | $$  /$$  \ $$      | $$            /$$  \ $$
+#  | $$     | $$$$$$$|  $$$$$$/  | $$ |  $$$$$$/      | $$$$$$$$     |  $$$$$$/
+#  |__/     |________/\______/   |__/  \______/       |________/      \______/ 
+#                                                                              
+#
+#            Community composition barcharts
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# Dataframe with taxrank mean relative abundance by (taxrank, city, date)
+which_taxrank <- 'Family'
+
+# relAb by taxrank by sample with relevant metadata
+melted <- imap(ps_rare.ls, function(ps, barcode) {
+  
+  psflashmelt(ps) %>%
+    # Sum abundance by taxrank for each sample
+    group_by(Sample, date, city, time, !!sym(which_taxrank)) %>% 
+    summarise(Abundance = sum(Abundance), .groups = 'drop') %>%
+    
+    # Then for all samples within a date/city/time combination
+    group_by(date, city, time, !!sym(which_taxrank)) %>% 
+    summarise(Abundance = sum(Abundance), .groups = 'drop') %>%
+    
+    # Compute relab
+    group_by(date, time, city) %>% 
+    mutate(relAb = Abundance/sum(Abundance)) %>% 
+    ungroup() %>% 
+    
+    # Select and format variables
+    select(!!sym(which_taxrank), relAb, city, time, date) %>% 
+    mutate(Barcode = barcode, 
+           # To prevent gap in date x axis, we recode date. 
+           date_label = paste(month(date, label = TRUE, abbr=TRUE), 
+                            day(date)),
+         # Recode date as character factor 
+         date = as.character(date))
+}) %>% list_rbind
+
+# Count number of sample by unique (taxrank, city, date) for each barcode
+sample_counts <- imap(ps_rare.ls, function(ps, barcode){
+  samdat_as_tibble(ps) %>% 
+    group_by(date, city, time) %>% 
+    summarise(n_samples = n(), .groups = 'drop') %>% 
+    mutate(Barcode = barcode,
+           date_label = paste(month(date, label = TRUE, abbr=TRUE), 
+                              day(date)),
+           date = as.character(date))
+}) %>% list_rbind
+
+# Compile community plots for each city 
 community_plots.ls <- map(cities, function(ci) {
   
   # Each timeslot needs to be created separately, to better manage the x (time) axis
@@ -187,19 +192,7 @@ community_plots.ls <- map(cities, function(ci) {
         as.character %>% # Set Others and Unclassified first:
         setdiff(., c('Others', 'Unclassified')) %>% c('Others', 'Unclassified', .)
       
-      # Is Unclassified part of the topN? 
-      if(('Unclassified' %in% top_taxa$aggTaxo)) {
-        fixed_colours <- c("#C6C2C2", "#E6E5C1")
-      } else {
-        fixed_colours <- c("#C6C2C2")
-      }
-      
-      # Define palette
-      expanded_palette <- colorRampPalette(brewer.pal(12, 'Set3'))(nTaxa) %>% 
-        setdiff(., fixed_colours) %>% # colours reserved
-        c(fixed_colours, .)
-      
-      # 
+      # Plot data
       plot.df <- melted_filt %>% 
         left_join(top_taxa %>% select(-relAb), by = which_taxrank) %>%
         group_by(date, time, Barcode, aggTaxo) %>% 
@@ -208,6 +201,7 @@ community_plots.ls <- map(cities, function(ci) {
         ) %>% # Filter at the end, to keep all taxa levels in the df
         filter(time == time_period)
       
+      # Count number of samples by sample groups
       sample_counts_filt <- sample_counts %>% 
         filter(city == ci & Barcode == barcode & time == time_period) %>% 
         select(date, n_samples)
@@ -224,11 +218,14 @@ community_plots.ls <- map(cities, function(ci) {
           vjust = -1,  size = 2,
           inherit.aes = FALSE  # Ignore fill aesthetic
         ) +
+        coord_cartesian(clip = "off") +  # Disable clipping
         facet_grid(Barcode~.) +
         scale_fill_manual(values = palettes[[barcode]]) +
-        scale_x_discrete(labels = date_labels, drop = FALSE) +
+        scale_x_discrete(labels = date_labels, drop = FALSE) + # 
+        scale_y_continuous(expand = expansion(mult = c(0, 0.05))) + # expand space between top of cols and panel border
         labs(fill = which_taxrank, y = '') +
         theme(panel.grid.minor = element_blank(),
+              panel.border = element_blank(),
               axis.ticks.x = element_blank(),
               panel.grid = element_blank(),
               strip.text = element_text(size = 13))
@@ -256,7 +253,7 @@ community_plots.ls <- map(cities, function(ci) {
       theme(axis.text.y = element_blank())
   ) %>% compact()
   
-  p_bact <- reduce(p_bact, `|`) + 
+  p_bact <- purrr::reduce(p_bact, `|`) + 
     plot_layout(guides = "collect") & 
     theme(axis.text.x = element_blank(),
           axis.title = element_blank())
@@ -264,7 +261,8 @@ community_plots.ls <- map(cities, function(ci) {
   # FUNGI (Middle)
   p_fung <- list(
     time_plots.ls[['Spring']][['FUNG']] + 
-      theme(strip.text.y = element_blank()),
+      theme(strip.text.y = element_blank()) +
+      labs(y = 'Mean relative abundance of amplicons'),
     
     if (ci != 'Quebec') {
     time_plots.ls[['Summer']][['FUNG']] +
@@ -278,7 +276,7 @@ community_plots.ls <- map(cities, function(ci) {
             axis.title = element_blank()) 
   ) %>% compact()
   
-  p_fung <- reduce(p_fung, `|`) + 
+  p_fung <- purrr::reduce(p_fung, `|`) + 
     plot_layout(guides = "collect") & 
     theme(axis.text.x = element_blank(),
           axis.title.x = element_blank())
@@ -299,7 +297,7 @@ community_plots.ls <- map(cities, function(ci) {
       theme(axis.text.y = element_blank())
   ) %>% compact
   
-  p_plan <- reduce(p_plan, `|`) + 
+  p_plan <- purrr::reduce(p_plan, `|`) + 
     plot_layout(guides = "collect") & 
     theme(axis.text.x = element_text(angle = 90),
           axis.title = element_blank())
@@ -312,120 +310,35 @@ community_plots.ls <- map(cities, function(ci) {
       legend.key.size = unit(12,"pt")) 
 })
 
-
-# === COMPUTE ALPHA DIVERSITY
-diversity.df <- imap(ps_rare.ls, function(ps, barcode) {
-  estimate_diversity(ps, 'Shannon') %>% 
-    data.frame(Shannon = .) %>% 
-    rownames_to_column('Sample') %>% 
-    left_join(samdat_as_tibble(ps), # add samdat
-              by = 'Sample') %>% 
-    mutate(Barcode = barcode,
-           Barcode = recode(Barcode, !!!barcodes),
-           time = factor(time, names(period_colours)),
-           Hill_1 = exp(Shannon))
-}) %>% list_rbind 
-  
-# Betadiv en BFP
-# Split every dataset by city
-ps_byCity.ls <- lapply(cities, function(city_name) { # 1st level: city
-  lapply(ps_rare.ls, function(ps) { # 2nd level: barcode
-    
-    # Subset samples by city
-    selected_samples <- rownames(sample_data(ps))[sample_data(ps)$city == city_name]
-    
-    # Subset the phyloseq object and remove zero-count taxa
-    prune_samples(selected_samples, ps) %>%
-      prune_taxa(taxa_sums(.) > 0, .)
-  })
-}); names(ps_byCity.ls) <- cities
-
-# Iterate permanova for each city, barcode, distance
-pcoa.ls <- imap(ps_byCity.ls, function(ps.ls, city) {
-  imap(ps.ls, function(ps, barcode){
-    out <- list()
-    out[['robust.aitchison']] <- compute_pcoa(ps, dist = 'robust.aitchison')
-    out[['bray']] <- compute_pcoa(ps, dist = 'bray')
-    out
-  })
-})
-
-# Compute eigenvalues and create strings for plots
-eig.df <- imap(pcoa.ls, function(pcoa_barcode.ls, city) {
-  imap(pcoa_barcode.ls, function(pcoa_dist.ls, barcode) {
-    imap(pcoa_dist.ls, function(pcoa, dist) {
-      pcoa$eig %>% 
-        data.frame(Eig = .) %>% 
-        rownames_to_column('MDS') %>% 
-        tibble %>% 
-        mutate(Barcode = barcode,
-               City = city,
-               Dist = dist) %>% 
-        group_by(Barcode, City, Dist) %>% 
-        mutate(Eig = 100*Eig/sum(Eig)) %>% # Compute %eig
-        filter(MDS %in% c('MDS1', 'MDS2')) # keep the 1st two
-    }) %>% list_rbind
-  }) %>% list_rbind
-}) %>% list_rbind %>% 
-  group_by(Barcode, City, Dist, MDS) %>%
-  mutate(Barcode = recode(Barcode, !!!barcodes),
-         MDS = case_when(MDS == 'MDS1' ~ 'PCo1',
-                         MDS == 'MDS2' ~ 'PCo2')) %>% 
-  summarize(Eig = paste0(MDS, ": ", round(Eig, 1), "%"), .groups = "drop") %>%
-  pivot_wider(names_from = MDS, values_from = Eig) 
-
-# Compile pcoa data 
-pcoa.df <- imap(pcoa.ls, function(pcoa_barcode.ls, city) {
-  imap(pcoa_barcode.ls, function(pcoa_dist.ls, barcode) {
-    imap(pcoa_dist.ls, function(pcoa, dist) {
-      pcoa$metadata %>% 
-        rownames_to_column('Sample') %>% 
-        select(Sample, time, PCo1, PCo2) %>% 
-        mutate(Barcode = barcode,
-               Dist = dist,
-               City = city) # add barcode name for each iteration
-    }) %>% list_rbind
-  }) %>% list_rbind
-}) %>% list_rbind %>% 
-  mutate(Barcode = recode(Barcode, !!!barcodes),
-         time = recode_factor(time, !!!c(
-             'Spring' = '1',
-             'Summer' = '2',
-             'Fall' = '3')),
-         time = factor(time, levels = c('1', '2', '3')))
-
-period_colour_num <- c(
-  '1' = unname(period_colours[1]),
-  '2' = unname(period_colours[2]),
-  '3' = unname(period_colours[3])
-)
-
-
 #=== ADD DIVERSITIES TO COMMUNITY PLOT
+alphadiv.df <- read_rds(file.path(urbanbio.path, 'data/diversity/alpha_diversity.rds'))
+betadiv.ls <- read_rds(file.path(urbanbio.path, 'data/diversity/beta_diversity.ls.rds'))
+
 map(cities, function(ci) {
   
-  adiv.plot <- diversity.df %>% 
+  adiv.plot <- alphadiv.df %>%
     filter(city == ci) %>% 
     ggplot(aes(x = time, y = Shannon, fill = factor(veg_index_bracket.))) + 
+    scale_fill_manual(values = nvdi_colours) +
     geom_boxplot(linewidth = 0.2) +
     facet_grid(.~Barcode) +
     labs(fill = 'Vegetation index') 
   
   bdiv.plot.ls <- map(barcodes, function(barcode) {
     
-    PCo <- eig.df %>% 
+    PCo <- betadiv.ls$eig.df %>% 
       filter(City == ci & Dist == 'bray' & Barcode == barcode) %>% 
       select(PCo1, PCo2) %>% as.vector
     
-    pcoa.df %>% 
+    betadiv.ls$plot.df %>% 
       filter(City == ci & Dist == 'bray' & Barcode == barcode) %>%
       ggplot(aes(x = PCo1, y = PCo2, colour = time)) +
       geom_point(size = 1) +
       stat_ellipse(level = 0.95, geom = 'polygon', 
                    alpha = 0.2, aes(fill = time)) +
       theme_light() +
-      scale_colour_manual(values = period_colour_num) +
-      scale_fill_manual(values = period_colour_num) +
+      scale_colour_manual(values = period_colours) +
+      scale_fill_manual(values = period_colours) +
       labs(colour = 'Sampling\nperiod', fill = 'Sampling\nperiod', 
            x = PCo$PCo1, y = PCo$PCo2) +
       theme(axis.title.y = element_text(margin = margin(r = -2, l=0)),
@@ -442,10 +355,9 @@ map(cities, function(ci) {
           legend.box.spacing = unit(0, "cm"),
     )
   
-  
    ggsave(paste0('~/Desktop/ip34/urbanBio/out/community', ci,'.pdf'),
           plot = plot, bg = 'white', width = 2000, height = 2600,
-          units = 'px', dpi = 180)
+          units = 'px', dpi = 160)
   
 })
 
