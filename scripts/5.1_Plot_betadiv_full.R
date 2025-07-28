925dc0004d2e1bde7f1ac65f547fbbb86b0d29eb
Isabelle Laforest-Lapointe <isabelle.laforest.lapointe@gmail.com>
2025-07-28 05:35:01 -0400

    New colours for city PCoA

--- scripts/5.1_Plot_betadiv_full.R
+++ scripts/5.1_Plot_betadiv_full.R
@@ -32,7 +32,10 @@ theme_set(theme_light())
 # From scripts/3_metrics.R
 betadiv.ls <- read_rds('data/diversity/beta_diversity_full.ls.rds')
 
-
+betadiv.ls$plot.df$time<-c(betadiv.ls$BACT$bray$metadata$time,betadiv.ls$BACT$bray$metadata$time,
+                           betadiv.ls$FUNG$bray$metadata$time,betadiv.ls$FUNG$bray$metadata$time,
+                           betadiv.ls$PLAN$bray$metadata$time,betadiv.ls$PLAN$bray$metadata$time)
+betadiv.ls$plot.df$time<-factor(betadiv.ls$plot.df$time,levels=levels(betadiv.ls$plot.df$time)[c(2,3,1)])
 bdiv.plot.ls <- map(kingdoms, function(barcode) {
   
   PCo <- betadiv.ls$eig.df %>% 
@@ -42,24 +45,28 @@ bdiv.plot.ls <- map(kingdoms, function(barcode) {
   betadiv.ls$plot.df %>% 
     filter(Dist == 'bray' & Barcode == barcode) %>%
     ggplot(aes(x = PCo1, y = PCo2, colour = city)) +
-    geom_point(size = 2, colour = 'black', shape = 21, aes(fill = city)) +
+    geom_point(size = 0.5, colour = 'black', aes(fill = city)) +
     stat_ellipse(level = 0.95, geom = 'polygon', 
-                 alpha = 0.2, aes(fill = city)) +
-  #  scale_fill_manual(values = period_colours) +
-    labs(colour = 'Sampling\nperiod', fill = 'Sampling\nperiod', 
+                 alpha = 0.2, aes(fill = city,colour = city)) +
+    scale_color_manual(values = c("black", "black", "black")) +
+    scale_fill_manual(values = c("#C60C30", "#1D4E89", "#2E8B57")) +
+    labs(colour = 'City', fill = 'City', shape ='Sampling\nperiod',
          x = PCo$PCo1, y = PCo$PCo2) +
     theme(axis.title.y = element_text(margin = margin(r = -2, l=0)),
-          axis.text = element_blank()) %>% 
+          axis.text = element_blank())+
+    geom_point(size = 3, colour = 'black', aes(fill = city,shape = time))+
+    scale_shape_manual(values=c(21,22,23))+
+    guides(fill="none",shape="none",colour="none")%>% 
     return() 
 })
 
 
 bdiv.plot.ls$BACT + bdiv.plot.ls$FUNG + bdiv.plot.ls$PLAN +
-  plot_layout(guides = 'collect')
+  plot_annotation(tag_levels = "A")
 
 # EXPORT
 ggsave(paste0('out/_MAIN/community_allCities.pdf'),
-       bg = 'white', width = 2800, height = 1000,
+       bg = 'white', width = 3000, height = 1000,
        units = 'px', dpi = 200)
 
 
