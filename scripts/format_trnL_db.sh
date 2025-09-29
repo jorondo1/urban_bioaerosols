a17232ef3b889b824369649f5058690b613bdca5
Jonathan Rondeau-Leclaire <jonathan.rondeau-leclaire@usherbrooke.ca>
2025-09-29 15:17:34 -0400

    cleanup

--- scripts/format_trnL_db.sh
+++ scripts/format_trnL_db.sh
@@ -1,3 +1,5 @@
+# From envBarcodeMiner database creation, format headers using this before passing to Dada2
+
 trnl_ref=/fast2/def-ilafores/envBarcodeMiner/trnL_CGAAATCGGTAGACGCTACG/hits.lineage.noacc.fa
 
 cat $trnl_ref | sed -E 's/^>(([^;]*;){5}[^;]*);.*/>\1;/' > /fast2/def-ilafores/envBarcodeMiner/trnL_CGAAATCGGTAGACGCTACG/hits.lineage.Genus.fa
