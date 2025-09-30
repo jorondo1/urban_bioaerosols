0271240238b9dc2f12151e919f0b35c014038c38
Jonathan Rondeau-Leclaire <jonathan.rondeau-leclaire@usherbrooke.ca>
2025-09-30 09:23:51 -0400

    clean for publish

--- scripts/format_trnL_db.sh
+++ scripts/format_trnL_db.sh
@@ -1,3 +1,6 @@
+# Database is created using script from this repository :
+
+
 # From envBarcodeMiner database creation, format headers using this before passing to Dada2
 
 trnl_ref=/fast2/def-ilafores/envBarcodeMiner/trnL_CGAAATCGGTAGACGCTACG/hits.lineage.noacc.fa
